de010d4a0a952e1aa6c33126df9ff1f90013abc4
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-14 09:16:32 -0600

    add photo of ERD to root of project

--- seeds/0_reset.js
+++ seeds/0_reset.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
-      return knex.raw('TRUNCATE author_book, book, author RESTART IDENTITY CASCADE')
+        return knex.raw('TRUNCATE author_book, book, author RESTART IDENTITY CASCADE')
       .then(function() {
         return knex('author_book').del()
       .then(function() {
